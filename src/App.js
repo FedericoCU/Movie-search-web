5eac575472bc660ff988ffd3d723189aeedb23ad
FedericoCU <fede.cdiaz@gmail.com>
2022-09-01 13:06:58 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchIcon from './search.svg'
 import MovieCard from './components/MovieCard';
 
 
-const API_URL = 'http://www.omdbapi.com?apikey=4204626';
+const API_URL = 'https://www.omdbapi.com?apikey=4204626';
 
 const App = () => {
 
